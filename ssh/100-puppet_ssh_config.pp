2ea61a824159855b06645861bcdb2eb36b7a4ba7
yvettegahamanyi <ygahamanyi26@gmail.com>
2024-07-12 23:36:46 +0200

    fix Client configuration file (w/ Puppet)

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,6 @@
+# This Puppet script configures the SSH client to use a specific private key
+# and disables password authentication.
+
 file_line { 'Turn off password authentication':
   path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
