79de301aff5363562f94e26e4680620e0c44e5a1
yvettegahamanyi <ygahamanyi26@gmail.com>
2024-07-12 23:30:44 +0200

    ft Client configuration file (w/ Puppet)

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+file_line { 'Turn off password authentication':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+  match => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#?IdentityFile',
+}
